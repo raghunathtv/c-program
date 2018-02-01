70ad5e0baf261a81c0a3da23e827caa5ec2cbb04
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-02-01 16:27:35 +0530

    Add files via upload

---
+++ main(2).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter n value");
+scanf("%d",&n);
+if(a%2==0)
+{
+a=a/2;
+if(a%2==0)
+{
+printf("the number is power of 2");
+}
+}
+getch();
+}
