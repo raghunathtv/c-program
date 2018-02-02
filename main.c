d1e89f56d2ccbeb80fdd8d9c35cacb14d6f76d6d
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-02-02 14:11:31 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,16 @@
-#include<stdio.h>
- 
+
+#include <stdio.h>
 int main()
 {
-  	char str1[50], str2[50];
-  	int i=0, c=0;
-  	printf("\nEnter first string :: ");
-  	gets(str1);
-  	printf("\nEnter second string :: ");
-  	gets(str2);
-  	printf("\nStrings are :: \n\n");
-    puts(str1);
-    puts(str2);
-	while((str1[i]!='\0') || (str2[i]!='\0'))
+    int n, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=1; i <= n; ++i)
     {
-    		if(str1[i]!=str2[i])
-      			c++;
-    		i++;
-   	}
-   	if(c==0)
-     		puts("\nStrings are equal.\n");
-   	else
-     		puts("\nStrings are not equal.\n");
- 
-   	return 0;
+        sum += i; 
+    }
+    printf("Sum = %d",sum);
+    return 0;
 }
+
 
