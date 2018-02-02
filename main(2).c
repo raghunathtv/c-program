40679a39c5e9eff55331392a3075ad6becb20e25
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-02-02 14:44:20 +0530

    Add files via upload

--- main(2).c
+++ main(2).c
@@ -1,17 +1,17 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+
+#include <stdio.h>
+int main()
 {
-int a;
-printf("enter n value");
-scanf("%d",&n);
-if(a%2==0)
-{
-a=a/2;
-if(a%2==0)
-{
-printf("the number is power of 2");
+  int x,y,temp;
+  printf("enter the values of x and y:");
+  scanf("%d%d",&x,&y);
+  printf("before swapping x=%d y=%d",x,y);
+  temp=x;
+  x=y;
+  y=temp;
+  printf("after swapping x=%d y=%d",x,y);
+
+    return 0;
 }
-}
-getch();
-}
+
+
