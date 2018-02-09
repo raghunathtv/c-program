3c356af9f1640db9415939708dd08cc1945813e5
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-02-09 14:08:14 +0530

    Add files via upload

---
+++ main(5).c
@@ -0,0 +1,10 @@
+
+ #include <stdio.h>
+ float main()
+{
+       float i;
+       printf("enter the float number");
+       scanf("%f",&i);
+       printf("round of  %f is  %f\n", i, round(i));
+       return 0;
+}
