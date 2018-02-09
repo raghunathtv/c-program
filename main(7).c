1039482a68d4f6278d26a941de70e6522f90a9ac
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-02-09 14:24:19 +0530

    Add files via upload

---
+++ main(7).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    int n1,n;
+    scanf("%d",&n);
+    n1=n+1;
+    while(n1!=0)
+    {
+        if(n1%10==0)
+        {
+            printf("%d",n1);
+            break;
+        }
+        n1++;
+        }
+  
+}
