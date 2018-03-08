c97330cccd60252f2998d573b38e25b7fb6a78a6
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-03-08 16:42:29 +0530

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a, n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(a = 0; a <= n; a++)
+    {
+        if (n == a * a)
+        {
+            printf("YES");
+            return 0;
+        }
+    }
+    printf("NO");
+    return 0;
+}
