946f7ea045c27e6c5e1629cb026da17eaf5193c4
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-03-09 15:47:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	char str[20];
+	int len;
+	cin.getline(str,100);
+	len=strlen(str);
+	if(len%2==0)
+	{
+	    str[len/2]='*';
+	    str[(len/2)-1]='*';
+	    cout<<str;
+	}
+	else
+	{
+	    str[len/2]='*';
+	    cout<<str;
+	}
+	exit 0;
+}
+
