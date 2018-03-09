7bdc8b18b4b27a8f6bd8a3458f7a88a365bfcc75
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-03-09 16:36:07 +0530

    Add files via upload

---
+++ main (9).c
@@ -0,0 +1,12 @@
+#pragma<stdio.h>
+int main()
+{
+	int l,b,h,tsa,volume;
+	scanf("%d %d %d",&l,&b,&h);
+	volume=l*b*h;
+	tsa=2*((l*b)+(b*h)+(h*l));
+	printf("volume=%d\n",volume);
+	printf("TSA=%d",tsa);
+	exit 0;
+}
+
