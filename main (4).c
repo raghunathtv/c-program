b45bf1903bc23a5350a35a2445fbc9368cba5188
raghunathtv <30828577+raghunathtv@users.noreply.github.com>
2018-03-13 16:00:34 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,16 @@
+#pragma<stdio.h>
+main()
+{
+int a,i,count=1;
+printf("Enter the number");
+scanf("%d",&a);
+for(i=2;i<a;i++)
+{
+if(a%i==0)
+ count++;
+ }
+ if(count!=1)
+ printf("yes");
+ else
+ printf("No");
+}
